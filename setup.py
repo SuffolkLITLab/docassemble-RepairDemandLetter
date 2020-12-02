f7e58465d818f047596d87eae7eefb097e144eaa
Christopher Selak <58575599+cselak@users.noreply.github.com>
2020-12-01 22:50:14 -0500

    Code updates perfect code running

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.RepairDemandLetter',
-      version='0.0.1',
+      version='0.0.2',
       description=('This interview helps create a letter to be used as a notification device for landlords demanding to make necessary repairs on or within the rental property. '),
-      long_description='# docassemble.RepairDemandLetter\n\nThis interview helps create a letter to be used as a notification device for landlords demanding to make necessary repairs on or within the rental property. \n\n## Author\n\nChristopher Selak, cselak@su.suffolk.edu\n\n',
+      long_description='# docassemble.RepairDemandLetter\r\n\r\nThis interview helps create a letter to be used as a notification device for landlords demanding to make necessary repairs on or within the rental property. \r\n\r\n## Author\r\n\r\nChristopher Selak, cselak@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Christopher Selak',
       author_email='cselak@su.suffolk.edu',
